0ecd684444c1f81a179e0956e0408c0fa4da4bb1
Tquillity <mikael.sundh@gmail.com>
2024-06-14 14:59:42 +0200

    Created files for block and blockchain routes and controllers as well as server.

---
+++ server/routes/block-routes.mjs
@@ -0,0 +1,8 @@
+// block-routes.mjs
+import express from 'express';
+import { mineBlock } from '../controllers/block-controller.mjs';
+
+const router = express.Router();
+
+router.route('/mine').post(mineBlock);
+export default router;
---
+++ server/server.mjs
@@ -0,0 +1,2 @@
+import express from 'express';
+import blockRouter from './routes/block-routes.mjs';
