cc378bdc677310d76f8713dd68ecbe98891e9db7
Tquillity <mikael.sundh@gmail.com>
2024-06-14 14:58:37 +0200

    added test and function for blockchain chain validation.

--- server/models/Blockchain.mjs
+++ server/models/Blockchain.mjs
@@ -1,4 +1,5 @@
 import Block from './Block.mjs';
+import { createHash } from '../utilities/crypto-lib.mjs';
 
 export default class Blockchain {
   constructor() {
@@ -15,5 +16,28 @@ export default class Blockchain {
     return newBlock;
   }
 
-  
+
+  static validateChain(chain) {
+    if (JSON.stringify(chain.at(0)) !== JSON.stringify(Block.genesis)) return false;
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, nonce, difficulty, data } = chain.at(i);
+      const currentLastHash = chain[i -1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
+
+      if (lastHash !== currentLastHash) return false;
+
+      if ( Math.abs(lastDifficulty - difficulty) > 1 ) return false;
+
+      const validHash = createHash(
+        timestamp,
+        lastHash,
+        nonce,
+        difficulty,
+        data
+      );
+      if (hash !== validHash) return false;
+    }
+    return true;
+  } 
 }
--- server/tests/blockchain.test.mjs
+++ server/tests/blockchain.test.mjs
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
 import Block from '../models/Block.mjs';
 import Blockchain from '../models/Blockchain.mjs';
+import { createHash } from '../utilities/crypto-lib.mjs';
 
 describe('Blockchain', () => {
   let blockchain, blockchain2, originalChain;
@@ -11,28 +12,95 @@ describe('Blockchain', () => {
     originalChain = blockchain.chain;
   });
 
-  // Test to verify that the blockchain has the property "chain"
-  it('Should have a property named "chain"', () => {
-    expect(blockchain).toHaveProperty('chain');
+  describe('Blockchain properties', () => { // Describe for code collaps and readability
+    // Test to verify that the blockchain has the property "chain"
+    it('Should have a property named "chain"', () => {
+      expect(blockchain).toHaveProperty('chain');
+    });
+
+    // Test to verify that the chain is of the type Array
+    it('Should have a property chain of type Array', () => {
+      expect(blockchain.chain).toBeInstanceOf(Array);
+    });
+
+    // Test to verify that the chain starts with the genesis block
+    it('Should start with the genesis block as the first block', () => {
+      expect(blockchain.chain.at(0)).toEqual(Block.genesis);
+    });
+
+    // Test to verify that the chain can be updated and added to
+    it('Should add a new block to the chain', () => {
+      const data = 'new block';
+      blockchain.addBlock({ data: data });
+
+      expect(blockchain.chain.at(-1).data).toEqual(data);
+    });
   });
 
-  // Test to verify that the chain is of the type Array
-  it('Should have a property chain of type Array', () => {
-    expect(blockchain.chain).toBeInstanceOf(Array);
+
+  describe('Validate chain', () => {
+
+    // Validate the genesis block
+    describe('when the chain does not start with the correct genesis block', () => {
+      it('Should return false', () => {
+        blockchain.chain[0] = { data: 'fake-genesis' };
+        expect(Blockchain.validateChain(blockchain.chain)).toBe(false);
+      });
+    });
+
+
+    describe('when the chain starts with the genesis block and has multiple blocks...', () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'Test text 1' });
+        blockchain.addBlock({ data: 'Test text 2' });
+        blockchain.addBlock({ data: 'Test text 3' });
+      });
+
+      describe('...and a lastHash reference has changed', () => {
+        it('Should return false', () => {
+          blockchain.chain[1].lastHash = 'broken-lastHash';
+          expect(Blockchain.validateChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe('...and the chain contains a block with invalid data', () => {
+        it('Should return false', () => {
+          blockchain.chain[2].data = 'broken-data';
+          expect(Blockchain.validateChain(blockchain.chain)).toBe(false);
+        });
+      });
+
+      describe('...and the chain contains a block with jumped difficulty', () => {
+        it('Should return false', () => {
+          const lastBlock = blockchain.chain.at(-1);
+          const lastHash = lastBlock.hash;
+          const timestamp = Date.now();
+          const nonce = 0 ;
+          const difficulty = lastBlock.difficulty - 4;
+          const data = [];
+          const hash = createHash(timestamp, lastHash, nonce, difficulty, data);
+
+          const block = new Block({
+            timestamp,
+            lastHash,
+            hash,
+            nonce,
+            difficulty,
+            data,
+          })
+
+          blockchain.chain.push(block);
+
+          expect(Blockchain.validateChain(blockchain.chain)).toBe(false);
+        });
+
+        describe('The chain is valid', () => {
+          it('Should return true', () => {
+            expect(Blockchain.validateChain(blockchain.chain)).toBe(true);
+          });
+        });
+      });
+
+    });
   });
-
-  // Test to verify that the chain starts with the genesis block
-  it('Should start with the genesis block as the first block', () => {
-    expect(blockchain.chain.at(0)).toEqual(Block.genesis);
-  });
-
-  // Test to verify that the chain can be updated and added to
-  it('Should add a new block to the chain', () => {
-    const data = 'new block';
-    blockchain.addBlock({ data: data });
-
-    expect(blockchain.chain.at(-1).data).toEqual(data);
-  });
-
-
 });
