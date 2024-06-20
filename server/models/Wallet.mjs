457eb5edd415dc39236a08526b2f993b7057e3fa
Tquillity <mikael.sundh@gmail.com>
2024-06-20 11:40:58 +0200

    Finished Wallet.mjs and corresponding tests wallet.test.mjs

--- server/models/Wallet.mjs
+++ server/models/Wallet.mjs
@@ -9,7 +9,51 @@ export default class Wallet {
     this.publicKey = this.keyPair.getPublic().encode('hex');
   }
 
+  // Static method to calculate the balance of a wallet based on the blockchain
+  static calculateBalance({ chain, address }) {
+    let total = 0;
+    let hasAddedTransaction = false;
 
+    // Iterate through each blick in the blockchain from latest to earliest
+    for (let i = chain.length -1; i > 0; i--) {
+      const block = chain[i];
+
+      // Iterate through each transaction in the block
+      for (let transaction of block.data) {
+        if (transaction.inputMap.address === address) {
+          hasAddedTransaction = true;
+        }
+
+        const value = transaction.outputMap[address];
+
+        if (value) {
+          total += value;
+        }
+      }
+
+      if(hasAddedTransaction) break;
+    }
+
+    return hasAddedTransaction ? total : INITIAL_BALANCE + total;
+  }
+
+  // Method to create a transaction
+  createTransaction({ recipient, amount, chain }) {
+    // Recalculate the balance if the blockchain is provided
+    if (chain) {
+      this.balance = Wallet.calculateBalance({
+        chain,
+        address: this.publicKey,
+      });
+    }
+
+    // Throw an error if the amount exceeds the balance
+    if (amount > this.balance) throw new Error('Amount exceeds balance');
+
+    return new Transaction({ sender: this, recipient, amount });
+  }
+
+  // method to sign data using the wallet's private key
   sign(data) {
     return this.keyPair.sign(createHash(data));
   }
--- server/tests/wallet.test.mjs
+++ server/tests/wallet.test.mjs
@@ -1,4 +1,4 @@
-import { it, describe, expect, beforeEach } from 'vitest';
+import { it, describe, expect, beforeEach, vi } from 'vitest';
 import Wallet from '../models/Wallet.mjs';
 import { verifySignature } from '../utilities/crypto-lib.mjs';
 import Transaction from '../models/Transaction.mjs';
@@ -46,4 +46,65 @@ describe('Wallet', () => {
     });
   });
 
+  //Describe tests related to creating transactions
+  describe('create transactions', () => {
+    // test to ensure an error is thrown when the amount exceeds the wallet balance
+    describe('and the amount is larger than the balance', () => {
+      it('should throw an error', () => {
+        expect(() => wallet.createTransaction({ amount: 999999, recipient:'Wrongela'})
+        ).toThrow('Amount exceeds balance');
+      });
+    });
+
+    // Test to ensure a valid transaction is created when the amount is valid
+    describe('and the amount is valid', () => {
+      let transaction, amount, recipient;
+
+      beforeEach(() => {
+        amount = 50;
+        recipient = 'Angela';
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+
+      // Test to ensure a Transaction object is created
+      it('should create a Transaction object', () => {
+        expect(transaction).toBeInstanceOf(Transaction);
+      });
+
+      // Test to ensure the wallet's inputmap matches the transaction's inputmap
+      it('should match the wallet inputMap', () => {
+        expect(transaction.inputMap.address).toEqual(wallet.publicKey);
+      });
+
+      // Test to ensure the amount is correctly output to the recipient
+      it('should output the amount to the recipient', () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
+
+      // Test to ensure Wallet.calculateBalance is called when a blockchain is supplied
+      describe('and a chain is supplied', () => {
+        it('should call the Wallet.calculateBalance()', () => {
+          const calculateBalanceMockFn = vi.fn();
+
+          // Save the original calculateBalance method
+          const originalCalculateBalance = Wallet.calculateBalance;
+          // Replace calculateBalance with a mock function
+          Wallet.calculateBalance = calculateBalanceMockFn;
+
+          // Create a transaction with a blockchain supplied
+          wallet.createTransaction({
+            recipient: 'Angela',
+            amount: 50,
+            chain: new Blockchain(),
+          });
+
+          // Check if the mock function was called
+          expect(calculateBalanceMockFn).toHaveBeenCalled();
+
+          // Restore the original calculateBalance method
+          Wallet.calculateBalance = originalCalculateBalance;
+        });
+      });
+    });
+  });
 });
