684a3709934a645149383131f9963f26404001f1
Tquillity <mikael.sundh@gmail.com>
2024-07-07 12:57:46 +0200

    Formating change for better overview to blockchain-controller export listBlock

--- server/controllers/blockchain-controller.mjs
+++ server/controllers/blockchain-controller.mjs
@@ -1,7 +1,9 @@
 import { blockchain } from '../server.mjs';
 
 export const listBlock = (req, res, next) => {
-  res
-  .status(200)
-  .json({ success: true, statusCode: 200, data: blockchain.chain });
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    data: blockchain.chain
+  });
 };
